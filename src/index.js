126326a039be983545dadad220daf490f9173bef
bklynate <nathanielcfa@gmail.com>
2018-06-03 12:01:36 -0400

    nate's solution to lesson-1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import http from 'http'
-import { createServer } from 'http'
+import http, { createServer } from 'http';
 
-import app from './server'
-let currentApp = app
+import app from './server';
 
+let currentApp = app;
+const server = http.createServer(app);
+
+server.listen(8080, () => 'Express server is live!');
 
 if (module.hot) {
-	module.hot.accept(['./server'], () => {
-		server.removeListener('request', currentApp)
-		server.on('request', app)
-		currentApp = app
-	})
+  module.hot.accept(['./server'], () => {
+    server.removeListener('request', currentApp);
+    server.on('request', app);
+    currentApp = app;
+  });
 }
